0784d11b99ba395707d04d402a80893836c7723c
LukasSeiler <63774117+LukasSeiler@users.noreply.github.com>
2021-10-10 18:57:37 +0200

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color: {
+        'custom-grey': '#222222'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
